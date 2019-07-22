3dae26e7549eff16772828fb5c34e7197b6cd9e9
disigma <yang.yu@disigma.org>
2019-07-22 15:59:37 +0800

    [logrotate] Add logrotate-3.15.0.
    - [popt] Add popt-1.16.
    
    https://github.com/logrotate/logrotate/releases/tag/3.15.0
    https://launchpad.net/popt/head/1.16/+download/popt-1.16.tar.gz

---
+++ logrotate/3.15.0/sources/test/test-0077.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. ./test-common.sh
+
+cleanup 77
+
+# ------------------------------- Test 77 ------------------------------------
+# ignore empty patterns given by the tabooext directive
+preptest test.log 77 1
+mkdir -p includedir
+cat > includedir/test-0077.conf << EOF
+copytruncate
+rotate 1
+EOF
+
+$RLR test-config.77 --force --verbose
+
+rm -rf includedir
+
+checkoutput <<EOF
+test.log 0
+test.log.1 0 zero
+EOF
